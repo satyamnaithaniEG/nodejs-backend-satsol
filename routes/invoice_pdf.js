2ad22d26fb88cd9487f7246e7cf68609ee2c5964
satyamnaithani <you@example.com>
2020-07-07 01:05:15 +0530

    Update

--- routes/invoice_pdf.js
+++ routes/invoice_pdf.js
@@ -7,24 +7,14 @@ const router = express.Router();
 const pdf = require('html-pdf');
 const pdfTemplate = require('../PdfFile/index');
 
-var options = {
-    format: 'A4',
-    width: "11in",
-    height: "17in",
-    // border: {
-    //     top: "3cm",            // default is 0, units: mm, cm, in, px
-    //     right: "0.5cm",
-    //     bottom: "3cm",
-    //     left: "0.5cm"
-    //   },
-};
+
 router.post('/create-pdf', (req, res) => {
-    pdf.create(pdfTemplate(req.body), options).toFile('routes/result.pdf', (err) => {
-        if (err) {
+    pdf.create(pdfTemplate(req.body), { format: 'A4', width:"11in", height:"17in" }).toFile('routes/result.pdf', (err) => {
+            if(err) {
             res.send(Promise.reject());
             console.log(err)
         }
-        // SalesController.helloWorld(req, res)
+       // SalesController.helloWorld(req, res)
         res.send(Promise.resolve());
     });
 });
